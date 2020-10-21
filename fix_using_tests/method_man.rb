ae75a9ea30284c871109e9c9a617e662a40327f2
Zipporah Horch <zipporahhorch@gmail.com>
2020-10-21 02:16:35 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{player2}"
 end
 
 def play_game(player2)
